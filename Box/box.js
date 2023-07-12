ef9ff2d00216e4bb71ab797fd328d6c6ae21590f
Nishu Pal <128295095+NishuPal23@users.noreply.github.com>
2023-07-12 22:11:18 +0530

    Add files via upload

---
+++ Box/box.js
@@ -0,0 +1,7 @@
+let one = document.getElementById("box");
+         box.addEventListener("click",a)
+        function a(){
+            one.style.height="600px";
+            one.style.width="600px";
+            one.style.backgroundColor="yellow";
+        }
