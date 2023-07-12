59d796775472d8abf6164ce6b2ca0a56ad4d59f7
Nishu Pal <128295095+NishuPal23@users.noreply.github.com>
2023-07-12 22:22:27 +0530

    Add files via upload

---
+++ ResNavbar/res.js
@@ -0,0 +1,7 @@
+let hem = document.getElementById("hem");
+let menu = document.getElementById("menu");
+hem.addEventListener("click",open);
+function open(){
+   menu.classList.toggle('show');
+            
+}
